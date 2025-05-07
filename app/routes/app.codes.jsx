6211564021d90216705d57761700b947126adc2f
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2025-05-07 13:50:04 +0300

    add placeholder code

--- app/routes/app.codes.jsx
+++ app/routes/app.codes.jsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Card,
   Layout,
   Link,
@@ -8,29 +9,37 @@ import {
   BlockStack,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
-import { useLoaderData } from "@remix-run/react";
 
-export async function loader() {
-  return [{"id": 1, "firstName": "Joe 1", "lastName": "Doe"},{"id": 2, "firstName": "Joe 2", "lastName": "Doe"},{"id": 3, "firstName": "Joe 3", "lastName": "Doe"}];
-}
-
-export default function Codes() {
-  const customers = useLoaderData();
-
+export default function AdditionalPage() {
   return (
     <Page>
-      <TitleBar title="Customer List" />
+      <TitleBar title="Additional page" />
       <Layout>
         <Layout.Section>
           <Card>
             <BlockStack gap="300">
-              <Text as="h2" variant="headingMd">Customers</Text>
+              <Text as="p" variant="bodyMd">
+                Tesitng
+              </Text>
+            </BlockStack>
+          </Card>
+        </Layout.Section>
+        <Layout.Section variant="oneThird">
+          <Card>
+            <BlockStack gap="200">
+              <Text as="h2" variant="headingMd">
+                Resources
+              </Text>
               <List>
-                {customers.map((customer) => (
-                  <List.Item key={customer.id}>
-                    {customer.firstName} {customer.lastName}
-                  </List.Item>
-                ))}
+                <List.Item>
+                  <Link
+                    url="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
+                    target="_blank"
+                    removeUnderline
+                  >
+                    App nav best practices
+                  </Link>
+                </List.Item>
               </List>
             </BlockStack>
           </Card>
