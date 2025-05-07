619a142047c63dd0f59452401dd2d530045a486b
Dangyras Stepanavicius <dangyrasstepanavicius@gmail.com>
2025-05-07 13:37:26 +0300

    add placeholder code

--- app/routes/app.codes.jsx
+++ app/routes/app.codes.jsx
@@ -1,5 +1,6 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import {
-  Box,
   Card,
   Layout,
   Link,
@@ -9,37 +10,47 @@ import {
   BlockStack,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
+import { authenticate } from "~/shopify.server";
+
+export async function loader({ request }) {
+  const { admin } = await authenticate.admin(request);
+
+  // const response = await admin.graphql(`
+  //   query CustomerList {
+  //     customers(first: 50) {
+  //       nodes {
+  //         id
+  //         firstName
+  //         lastName
+  //       }
+  //     }
+  //   }
+  // `);
+
+  // const data = await response.json();
+
+  // return json(data.data.customers.nodes);
+
+  return [{"id": 1, "firstName": "Joe 1", "lastName": "Doe"},{"id": 2, "firstName": "Joe 2", "lastName": "Doe"},{"id": 3, "firstName": "Joe 3", "lastName": "Doe"}]
+}
 
 export default function Codes() {
+  const customers = useLoaderData();
+
   return (
     <Page>
-      <TitleBar title="Codes" />
+      <TitleBar title="Customer List" />
       <Layout>
         <Layout.Section>
           <Card>
             <BlockStack gap="300">
-              <Text as="p" variant="bodyMd">
-                Tesitng
-              </Text>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section variant="oneThird">
-          <Card>
-            <BlockStack gap="200">
-              <Text as="h2" variant="headingMd">
-                Resources
-              </Text>
+              <Text as="h2" variant="headingMd">Customers</Text>
               <List>
-                <List.Item>
-                  <Link
-                    url="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
-                    target="_blank"
-                    removeUnderline
-                  >
-                    App nav best practices
-                  </Link>
-                </List.Item>
+                {customers.map((customer) => (
+                  <List.Item key={customer.id}>
+                    {customer.firstName} {customer.lastName}
+                  </List.Item>
+                ))}
               </List>
             </BlockStack>
           </Card>
